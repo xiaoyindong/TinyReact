121fff96c79f5fd3ec4404254e0a874eee479229
隐冬 <812748570@qq.com>
2021-05-24 16:34:20 +0800

    相同类型节点比对

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,12 +1,19 @@
 import mountElement from './mountElement';
 import updateTextNode from './updateTextNode';
+import updateNodeElement from './updateNodeElement';
+import createDOMElement from './createDOMElement';
 
-export default function diff (virtualDOM, container, oldDOM) {
+export default function diff(virtualDOM, container, oldDOM) {
     // 获取老的虚拟DOM对象
     const oldVirtualDOM = oldDOM && oldDOM._virtualDOM;
     // 判断oldDOM是否在巡
     if (!oldDOM) {
         return mountElement(virtualDOM, container);
+    } else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM.type !== 'function') {
+        // 如果标签类型不相同，并且不是组件。
+        const newElement = createDOMElement(virtualDOM);
+        // 替换DOM元素
+        oldDOM.parentNode.replaceChild(oldDOM, newElement);
     } else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
         // 两个元素类型相同，需要判断是文本类型节点还是元素类型节点
         // 文本类型直接更新内容
@@ -14,9 +21,10 @@ export default function diff (virtualDOM, container, oldDOM) {
         if (virtualDOM.type === 'text') {
             // 更新内容
             updateTextNode(virtualDOM, oldVirtualDOM, oldDOM);
-
         } else {
             // 更新元素属性
+            // 要更新的哪个元素，更新的虚拟DOM，旧的虚拟DOM
+            updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM)
         }
         // 遍历子元素进行对比
         virtualDOM.children.forEach((child, i) => {
--- src/index.js
+++ src/index.js
@@ -15,21 +15,42 @@ const virtualDOM = (
   </div>
 )
 
+const modifyDOM = (
+  <div className="container">
+    <h1>你好 我是新的虚拟DOM</h1>
+    <h2 className="test">有class名称的标签</h2>
+    {
+        1 === 2 && <h1>布尔值节点</h1>
+    }
+    <button onClick={() => {  alert("hello")}}>点击事件</button>
+    <br />
+    <input value="value" />
+    <br />
+    设置checked<input type="checkbox" checked="checked" />
+  </div>
+)
+
 const root = document.getElementById('root');
 
-function Head (props) {
-  return <div>
-    {props.title}
-  </div>
-}
+// function Head (props) {
+//   return <div>旧的</div>
+// }
+// function Head2 (props) {
+//   return <div>新的</div>
+// }
 
-class Alert extends TinyReact.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <div>{this.props.name} {this.props.age}</div>
-  }
-}
 
-TinyReact.render(<Alert name="yindong" age={18}/>, root);
+// class Alert extends TinyReact.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return <div>{this.props.name} {this.props.age}</div>
+//   }
+// }
+
+TinyReact.render(virtualDOM, root);
+
+setTimeout(() => {
+  TinyReact.render(modifyDOM, root);
+}, 3000)
